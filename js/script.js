0d36d0bb18f6930243725ad6b2f3c9513f7fc1b5
Lukasz Lucko <lukaszl95@o2.pl>
2017-12-20 14:27:14 +0100

    Zadanie4

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = prompt('a');
+var h = prompt('h');
+var triangleArea;
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
